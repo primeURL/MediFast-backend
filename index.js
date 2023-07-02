dd1ef93040f97f672df6a0823fa73d701c08d066
Utkarsh Lanjewar <deblinaroy066@gmail.com>
2023-07-02 11:39:20 +0530

    Initial Setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const connectToMongo = require('./db')
+
+require('dotenv').config()
+const app = express();
+connectToMongo()
+app.use(cors())
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+
+app.get('/',(req,res)=>{
+    res.send('Inside index.js')
+})
+
+app.listen(8080,()=>console.log('Server started at port 8080'))
