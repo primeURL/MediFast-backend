07b49292ac256473d89a0950a2d0e1cbbdcb6886
Utkarsh Lanjewar <lanjewarutkarsh@gmail.com>
2023-07-30 20:39:44 +0530

    added stripe

--- routes/bookedDoctor.js
+++ routes/bookedDoctor.js
@@ -2,26 +2,43 @@ const router = require("express").Router();
 const BookedDoctorSchema = require('../models/BookedDoctors')
 const AddDoctorSchema = require('../models/addDoctorSchema')
 const sendEmail = require('../utils/sendEmail')
+const {v4 : uuidv4} = require('uuid')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 // This Route is to store booking of Doctors
 router.post("/", async (req, res) => {
-    const {doctorId,patientName,patientIssue,patientPhone,patientTime} = req.body
+    const {doctorId,patientName,patientIssue,patientEmail,patientTime,totalAmount,token} = req.body
 	try {
-        const response = await new BookedDoctorSchema(req.body)
-        response.save()
-        const data = await AddDoctorSchema.findOne({_id:doctorId})
-        const text = ` <div style="margin: auto;border-radius:10px;font-family: sans-serif; width: 500px;box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;">
-        <div style="text-align: center; padding: 10px;">
-            <h1 style="color: rgb(218, 94, 94);">Your Patient Details</h1>
-            <p>Patient Name : <b>${patientName}</b></p>
-            <p>Patient Issue : <b>${patientIssue}</b></p>
-            <p>Patient Mobile : <b>${patientPhone}</b></p>
-            <p>Appointment Time : <b>${patientTime}</b> </p>    
-        </div>
-        <p style="text-align: center; padding:10px; font-weight: 300;color: coral;">To Accept An Appointment Login to your Profile Page</p>
-                         </div>`
-        await sendEmail(data.email,'You have Appointment from Patient','Good Morning',text)
-        res.status(200).send('You Appointment Booked')
+        const customer = await stripe.customers.create({
+            email : token.email,
+            source : token.id
+        })
+        const payment = await stripe.paymentIntents.create({
+            amount : totalAmount * 100,
+            customer : customer.id,
+            currency : 'inr',
+            receipt_email : token.email
+        },{
+            idempotencyKey : uuidv4()
+        })
+        if(payment){
+            const response = await new BookedDoctorSchema(req.body)
+            response.save()
+            const data = await AddDoctorSchema.findOne({_id:doctorId})
+            const text = ` <div style="margin: auto;border-radius:10px;font-family: sans-serif; width: 500px;box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;">
+            <div style="text-align: center; padding: 10px;">
+                <h1 style="color: rgb(218, 94, 94);">Your Patient Details</h1>
+                <p>Patient Name : <b>${patientName}</b></p>
+                <p>Patient Issue : <b>${patientIssue}</b></p>
+                <p>Patient Email : <b>${patientEmail}</b></p>
+                <p>Appointment Time : <b>${patientTime}</b> </p>    
+            </div>
+            <p style="text-align: center; padding:10px; font-weight: 300;color: coral;">To Accept An Appointment Login to your Profile Page</p>
+                             </div>`
+            await sendEmail(data.email,'You have Appointment from Patient','Good Morning',text)
+            res.status(200).send('You Appointment Booked')
+        }
+       
     } catch (error) {
         res.status(500).send(error)
     }
